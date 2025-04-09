08802901be6aaad7707cbefc4164cfbc4d33db24
tarush <tarushruhela1234@gmail.com>
2025-04-09 16:46:52 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+import dotenv from "dotenv";
+dotenv.config({
+    path: './.env'
+});
+
+import express from 'express';
+import connectDB from './config/db.js';
+import authRoutes from './routes/auth.js';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+
+app.get('/', (req, res) => {
+    res.send("server is running");
+})
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send({ message: err.message || "Something went wrong!" });
+});
+
+
+
+connectDB()
+.then(()=>{
+    app.listen(port , ()=>{
+        console.log(`server is listening at port ${port}`);
+    })
+})
+.catch((err)=>{
+    console.log("MONGO db connection failed !!! ", err);
+})
---
+++ routes/auth.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import User from '../models/user.js'
+
+const router = express.Router();
+
+router.post('/register', async (req, res) => {
+  const { name, email, password, role } = req.body;
+  const hashed = await bcrypt.hash(password, 10);
+  const user = new User({ name, email, password: hashed, role });
+  await user.save();
+  res.json({ message: 'Registered' });
+});
+
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) return res.status(400).json({ msg: 'Invalid credentials' });
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) return res.status(400).json({ msg: 'Invalid credentials' });
+
+  const token = jwt.sign({ id: user._id, role: user.role }, 'secret');
+  res.json({ token, role: user.role });
+});
+
+export default router;
