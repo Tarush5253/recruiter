32c32ed9a70edc028e3ad20c12c5c29293c4f798
tarush <tarushruhela1234@gmail.com>
2025-04-09 23:09:42 +0530

    ..

--- index.js
+++ index.js
@@ -23,7 +23,8 @@ app.use(function (req, res, next) {
   app.use((req, res, next) => {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-  });
+  });}
+)
 
 app.use('/api/auth', authRoutes);
 
